feb168bc4311e2c6ea3b6d9eeb5d2c5cc6461803
samu235 <samuel2351994@hotmail.es>
2022-07-29 13:44:14 +0200

    add default img and styles img

--- components/CartIceCream.js
+++ components/CartIceCream.js
@@ -1,10 +1,20 @@
-export default function CartIceCream(props){
+import { useId } from "react"
+import styles from "../styles/CartIceCream.module.css"
 
-let urlPicture = props?.picture ? process.env.NEXT_PUBLIC_API+"/"+props?.picture :""
+export default function CartIceCream(props) {
 
-    return(
+    const imgDefault = "/gelado.png"
+    let urlPicture = props?.picture ? process.env.NEXT_PUBLIC_API + "/" + props?.picture : imgDefault
+    const idImg = "image" + useId()
+
+    function Error() {
+        console.log("error imagen", idImg)
+        document.getElementById(idImg).src = imgDefault
+    }
+
+    return (
         <div>
-            <img src={urlPicture}/>
+            <img id={idImg} className={styles.img} src={urlPicture} onError={Error} />
             <h3>{props?.title}</h3>
         </div>
     )
